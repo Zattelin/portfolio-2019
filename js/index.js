5c77f392b6fec4b670e0d404fb8d0b530d4e0d32
Zattelin <37785003+Zattelin@users.noreply.github.com>
2019-07-23 15:05:32 -0400

    added new graphic design page

--- js/index.js
+++ js/index.js
@@ -197,4 +197,40 @@ $(document).ready(function() {
 
   // });
 
+  /* Video Popup*/
+  // Define App Namespace
+  var popup = {
+    // Initializer
+    init: function() {
+      popup.popupVideo();
+    },
+    popupVideo : function() {
+
+    $('.video_model').magnificPopup({
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+    gallery: {
+          enabled:true
+        }
+  });
+
+/* Image Popup*/ 
+ $('.gallery_container').magnificPopup({
+      delegate: 'a',
+    type: 'image',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+    gallery: {
+          enabled:true
+        }
+  });
+
+    }
+  };
+  popup.init($);
 });
